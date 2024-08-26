03319ce2c75e886293303198f297e6825af16dd2
playforest <playforest0x@gmail.com>
2024-08-26 19:10:21 +1000

    add utils: export cpu profile logs

--- utils/utils.go
+++ utils/utils.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"runtime"
 	"runtime/pprof"
+	"time"
 )
 
 func SaveCache(filename string, data interface{}) {
@@ -69,4 +72,31 @@ func WriteMemoryProfile(filename string) {
 	if err := pprof.WriteHeapProfile(f); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func ExportTopFunctionsByTime(cpuProfilePath, outputDir string, topCount int) {
+	timestamp := time.Now().Format("2006-01-02_15-04-05")
+	outputFileName := fmt.Sprintf("top_%d_functions_by_time_%s.txt", topCount, timestamp)
+	outputPath := filepath.Join(outputDir, outputFileName)
+
+	// Ensure the output directory exists
+	if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
+		log.Printf("Error creating directory: %v", err)
+		return
+	}
+
+	cmd := exec.Command("go", "tool", "pprof", "-top", "-cum", fmt.Sprintf("-nodecount=%d", topCount), cpuProfilePath)
+	output, err := cmd.Output()
+	if err != nil {
+		log.Printf("Error running pprof: %v", err)
+		return
+	}
+
+	err = os.WriteFile(outputPath, output, 0644)
+	if err != nil {
+		log.Printf("Error writing to file: %v", err)
+		return
+	}
+
+	fmt.Printf("Top %d functions by time exported to %s\n", topCount, outputPath)
 }
